bcb27a00c88ca55554a0aa912fc075baeafd86e1
Steffen Imhof <mail@steffen-imhof.de>
2013-12-03 22:21:31 +0100

    Moved existing tests into unittests. Extended them a bit.

--- aclgrep.py
+++ aclgrep.py
@@ -31,8 +31,11 @@ def ip_to_bits(address):
 	# Fixup IP addresses with leading zeros
 	fixed_address = ".".join([str(int(x)) for x in address.split(".")])
 	
-	return struct.unpack("!L", socket.inet_aton(fixed_address))[0]
-	
+	try:
+		return struct.unpack("!L", socket.inet_aton(fixed_address))[0]
+	except socket.error:
+		raise ValueError("Invalid IP address")
+
 def ip_in_net(ip, net):
 	'''Checks if an IP adress is contained in a network described by a pair (net address, subnetmask).
 	   All values are given as longs.'''
@@ -46,7 +49,7 @@ def ip_and_mask_to_pair(pattern):
 	net = ip_to_bits(parts[0])
 	net_or_wildcard = ip_to_bits(parts[1])
 	
-	# special case full bits -> wildcard mask
+	# special case full bits -> subnet mask
 	if 0xffffffff == net_or_wildcard:
 		return (net, 0xffffffff)
 
@@ -66,83 +69,68 @@ def ip_and_cidr_to_pair(pattern):
 	wildcard = (1 << (32-int(parts[1])))-1
 	return (net, 0xffffffff ^ wildcard)
 
-def tests():
-	'''Run a few tests.'''
-	bit_print_pair(ip_and_mask_to_pair("192.168.2.0 255.255.255.0"))
-	bit_print_pair(ip_and_mask_to_pair("192.168.2.0/255.255.255.0"))
-	bit_print_pair(ip_and_mask_to_pair("192.168.2.0 0.0.0.255"))
-	bit_print_pair(ip_and_mask_to_pair("192.168.2.0 255.255.252.0"))
-	bit_print_pair(ip_and_mask_to_pair("10.0.0.0 255.0.0.0"))
+if __name__ == '__main__':
+	# check command line args
+	if len(sys.argv) < 2:
+		print "USAGE: aclgrep.py [-any] ip_adress file [, file, file, ...]"
+		sys.exit()
 
-	bit_print_pair(ip_and_cidr_to_pair("192.168.2.0/24"))
-	bit_print_pair(ip_and_cidr_to_pair("192.168.2.0/22"))
-	bit_print_pair(ip_and_cidr_to_pair("10.0.0.0/8"))
+	match_any = False
+	if sys.argv[1] == "-any":
+		ip_string = sys.argv[2]
+		match_any = True
+	else:
+		ip_string = sys.argv[1]
 
-# check command line args
-if len(sys.argv) == 2 and sys.argv[1] == "-test":
-	tests()
-	sys.exit()
+	ip_address = ip_to_bits(ip_string)
 
-if len(sys.argv) < 2:
-	print "USAGE: aclgrep.py [-any] ip_adress file [, file, file, ...]"
-	sys.exit()
+	# compile all patterns to regexes
+	mask_patterns    = [ re.compile(p) for p in mask_patterns ]
+	cidr_patterns    = [ re.compile(p) for p in cidr_patterns ]
+	aclname_patterns = [ re.compile(p) for p in aclname_patterns ]
 
-match_any = False
-if sys.argv[1] == "-any":
-	ip_string = sys.argv[2]
-	match_any = True
-else:
-	ip_string = sys.argv[1]
+	last_aclname = "(unknown)"
 
-ip_address = ip_to_bits(ip_string)
-
-# compile all patterns to regexes
-mask_patterns    = [ re.compile(p) for p in mask_patterns ]
-cidr_patterns    = [ re.compile(p) for p in cidr_patterns ]
-aclname_patterns = [ re.compile(p) for p in aclname_patterns ]
-
-last_aclname = "(unknown)"
-
-# check all lines in all files (or stdin)
-for line in fileinput.input(sys.argv[(2+match_any):]):
-	line_has_matched = False
+	# check all lines in all files (or stdin)
+	for line in fileinput.input(sys.argv[(2+match_any):]):
+		line_has_matched = False
 	
-	# check for ACL name
-	for p in aclname_patterns:
-		m = p.search(line)
-		if m:
-			last_aclname = m.group(2)
+		# check for ACL name
+		for p in aclname_patterns:
+			m = p.search(line)
+			if m:
+				last_aclname = m.group(2)
+				continue
+
+		# check any if desired
+		if match_any and "any" in line:
+			print fileinput.filename() + " (" + last_aclname + "):" + line,
 			continue
 
-	# check any if desired
-	if match_any and "any" in line:
-		print fileinput.filename() + " (" + last_aclname + "):" + line,
-		continue
+		# check for the IP address directly first
+		if ip_string in line:
+			print fileinput.filename() + ":" + line,
+			continue
 
-	# check for the IP address directly first
-	if ip_string in line:
-		print fileinput.filename() + ":" + line,
-		continue
-
-	for p in mask_patterns:
-		m = p.search(line)
-		while m:
-			line_has_matched = True
-			net = ip_and_mask_to_pair(m.group(1))
-			if ip_in_net(ip_address, net):
-				print fileinput.filename() + " (" + last_aclname + "):" + line,
-				break
-			m = p.search(line, m.start() + 1)
+		for p in mask_patterns:
+			m = p.search(line)
+			while m:
+				line_has_matched = True
+				net = ip_and_mask_to_pair(m.group(1))
+				if ip_in_net(ip_address, net):
+					print fileinput.filename() + " (" + last_aclname + "):" + line,
+					break
+				m = p.search(line, m.start() + 1)
 	
-	# prevent CIDR matches if a mask match was already found
-	if line_has_matched:
-		continue
+		# prevent CIDR matches if a mask match was already found
+		if line_has_matched:
+			continue
 	
-	for p in cidr_patterns:
-		m = p.search(line)
-		while m:
-			net = ip_and_cidr_to_pair(m.group(1))
-			if ip_in_net(ip_address,net):
-				print fileinput.filename()+ " (" + last_aclname + "):" + line,
-				break
-			m = p.search(line, m.start() + 1)
+		for p in cidr_patterns:
+			m = p.search(line)
+			while m:
+				net = ip_and_cidr_to_pair(m.group(1))
+				if ip_in_net(ip_address,net):
+					print fileinput.filename()+ " (" + last_aclname + "):" + line,
+					break
+				m = p.search(line, m.start() + 1)
