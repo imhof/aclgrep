6bc6a95e67d6924b65e3384bf2a843cd6916c1d4
Steffen Imhof <mail@steffen-imhof.de>
2013-12-04 23:17:06 +0100

    Move logic into class.
    - Fixed an output issue on the way.
    - Added some PyCharm config files?!

--- aclgrep.py
+++ aclgrep.py
@@ -5,66 +5,127 @@
 import socket, struct, sys, re, fileinput
 from optparse import OptionParser
 
-# Configuration
-# Add special patterns to detect IP networks here
-cidr_patterns = [
-    r"\D(\d+\.\d+\.\d+\.\d+\/\d+)\D",
-]
+class ACLGrepper:
+    '''The main class which handles the grep process as a whole.'''
 
-mask_patterns = [
-    r"\D(\d+\.\d+\.\d+\.\d+\D\d+\.\d+\.\d+\.\d+)\D",
-]
+    # Configuration
+    # Add special patterns to detect IP networks here
+    cidr_patterns = [
+        r"\D(\d+\.\d+\.\d+\.\d+\/\d+)\D",
+    ]
+    mask_patterns = [
+        r"\D(\d+\.\d+\.\d+\.\d+\D\d+\.\d+\.\d+\.\d+)\D",
+    ]
+    aclname_patterns = [
+        r"ip access-list( extended)? (.*)$",
+    ]
+    splitter = re.compile(r"[^0-9.]")
 
-aclname_patterns = [
-    r"ip access-list( extended)? (.*)$",
-]
+    last_aclname = "(unknown)"
 
-splitter = re.compile(r"[^0-9.]")
+    ip_string = ""
+    ip_address = 0
+    match_any = False
 
 
-def ip_to_bits(address):
-    '''Turns an IP address in dot notation into a single long value.'''
+    def __init__(self, ip_string, match_any):
+        # compile all patterns to regexes
+        self.mask_patterns    = [ re.compile(p) for p in self.mask_patterns ]
+        self.cidr_patterns    = [ re.compile(p) for p in self.cidr_patterns ]
+        self.aclname_patterns = [ re.compile(p) for p in self.aclname_patterns ]
 
-    # Fixup IP addresses with leading zeros
-    fixed_address = ".".join([str(int(x)) for x in address.split(".")])
+        self.ip_string = ip_string
+        self.ip_address = self.ip_to_bits(ip_string)
 
-    try:
-        return struct.unpack("!L", socket.inet_aton(fixed_address))[0]
-    except socket.error:
-        raise ValueError("Invalid IP address")
+        self.match_any = match_any
 
-def ip_in_net(ip, net):
-    '''Checks if an IP adress is contained in a network described by a pair (net address, subnetmask).
-       All values are given as longs.'''
-    return (net[0] & net[1] == ip & net[1])
+    def ip_to_bits(self, address):
+        '''Turns an IP address in dot notation into a single long value.'''
 
-def ip_and_mask_to_pair(pattern):
-    '''Takes a mask pattern and creates a pair (net address, subnetmask) from it.
-       Detects automatically if the mask is a subnetmask or a wildcard mask, assuming the bits are
-       set continuously in either.'''
-    parts = re.split(splitter, pattern)
-    net = ip_to_bits(parts[0])
-    net_or_wildcard = ip_to_bits(parts[1])
+        # Fixup IP addresses with leading zeros
+        fixed_address = ".".join([str(int(x)) for x in address.split(".")])
 
-    # special case full bits -> subnet mask
-    if 0xffffffff == net_or_wildcard:
-        return (net, 0xffffffff)
+        try:
+            return struct.unpack("!L", socket.inet_aton(fixed_address))[0]
+        except socket.error:
+            raise ValueError("Invalid IP address")
 
-    # check if the mask is really a mask (only set bits from the right or left)
-    if net_or_wildcard & (net_or_wildcard + 1) != 0:
-        net_or_wildcard = 0xffffffff ^ net_or_wildcard
+    def ip_in_net(self, ip, net):
+        '''Checks if an IP adress is contained in a network described by a pair (net address, subnetmask).
+           All values are given as longs.'''
+        return (net[0] & net[1] == ip & net[1])
+
+    def ip_and_mask_to_pair(self, pattern):
+        '''Takes a mask pattern and creates a pair (net address, subnetmask) from it.
+           Detects automatically if the mask is a subnetmask or a wildcard mask, assuming the bits are
+           set continuously in either.'''
+        parts = re.split(self.splitter, pattern)
+        net = self.ip_to_bits(parts[0])
+        net_or_wildcard = self.ip_to_bits(parts[1])
+
+        # special case full bits -> subnet mask
+        if 0xffffffff == net_or_wildcard:
+            return (net, 0xffffffff)
+
+        # check if the mask is really a mask (only set bits from the right or left)
         if net_or_wildcard & (net_or_wildcard + 1) != 0:
-            # it's not, never match
-            return (0, 0xffffffff)
+            net_or_wildcard = 0xffffffff ^ net_or_wildcard
+            if net_or_wildcard & (net_or_wildcard + 1) != 0:
+                # it's not, never match
+                return (0, 0xffffffff)
 
-    return (net, 0xffffffff ^ net_or_wildcard)
+        return (net, 0xffffffff ^ net_or_wildcard)
 
-def ip_and_cidr_to_pair(pattern):
-    '''Takes a CIDR pattern and creates a pair (net address, subnetmask) from it.'''
-    parts = pattern.split("/")
-    net = ip_to_bits(parts[0])
-    wildcard = (1 << (32-int(parts[1])))-1
-    return (net, 0xffffffff ^ wildcard)
+    def ip_and_cidr_to_pair(self, pattern):
+        '''Takes a CIDR pattern and creates a pair (net address, subnetmask) from it.'''
+        parts = pattern.split("/")
+        net = self.ip_to_bits(parts[0])
+        wildcard = (1 << (32-int(parts[1])))-1
+        return (net, 0xffffffff ^ wildcard)
+
+    def grep(self, line):
+        line_has_matched = False
+
+        # check for ACL name
+        for p in self.aclname_patterns:
+            m = p.search(line)
+            if m:
+                self.last_aclname = m.group(2).strip()
+                continue
+
+        # check any if desired
+        if self.match_any and "any" in line:
+            print ("%s (%s): %s" % (fileinput.filename(), self.last_aclname , line)),
+            return
+
+        # check for the IP address directly first
+        if self.ip_string in line:
+            print fileinput.filename() + ":" + line,
+            return
+
+        for p in self.mask_patterns:
+            m = p.search(line)
+            while m:
+                line_has_matched = True
+                net = self.ip_and_mask_to_pair(m.group(1))
+                if self.ip_in_net(self.ip_address, net):
+                    print fileinput.filename() + " (" + self.last_aclname + "):" + line,
+                    break
+                m = p.search(line, m.start() + 1)
+
+        # prevent CIDR matches if a mask match was already found
+        if line_has_matched:
+            return
+
+        for p in self.cidr_patterns:
+            m = p.search(line)
+            while m:
+                net = self.ip_and_cidr_to_pair(m.group(1))
+                if self.ip_in_net(self.ip_address,net):
+                    print fileinput.filename() + " (" + self.last_aclname + "):" + line,
+                    break
+                m = p.search(line, m.start() + 1)
+
 
 if __name__ == '__main__':
     # check command line args
@@ -77,56 +138,9 @@ if __name__ == '__main__':
         print "USAGE: aclgrep.py [-a|--any] ip_adress file [, file, file, ...]"
         sys.exit()
 
-    ip_string = args.pop(0)
-    ip_address = ip_to_bits(ip_string)
+    # initialize grepper and...
+    grepper = ACLGrepper(args.pop(0), options.match_any)
 
-    # compile all patterns to regexes
-    mask_patterns    = [ re.compile(p) for p in mask_patterns ]
-    cidr_patterns    = [ re.compile(p) for p in cidr_patterns ]
-    aclname_patterns = [ re.compile(p) for p in aclname_patterns ]
-
-    last_aclname = "(unknown)"
-
-    # check all lines in all files (or stdin)
+    # ...check all lines in all files (or stdin)
     for line in fileinput.input(args):
-        line_has_matched = False
-
-        # check for ACL name
-        for p in aclname_patterns:
-            m = p.search(line)
-            if m:
-                last_aclname = m.group(2)
-                continue
-
-        # check any if desired
-        if options.match_any and "any" in line:
-            print fileinput.filename() + " (" + last_aclname + "):" + line,
-            continue
-
-        # check for the IP address directly first
-        if ip_string in line:
-            print fileinput.filename() + ":" + line,
-            continue
-
-        for p in mask_patterns:
-            m = p.search(line)
-            while m:
-                line_has_matched = True
-                net = ip_and_mask_to_pair(m.group(1))
-                if ip_in_net(ip_address, net):
-                    print fileinput.filename() + " (" + last_aclname + "):" + line,
-                    break
-                m = p.search(line, m.start() + 1)
-
-        # prevent CIDR matches if a mask match was already found
-        if line_has_matched:
-            continue
-
-        for p in cidr_patterns:
-            m = p.search(line)
-            while m:
-                net = ip_and_cidr_to_pair(m.group(1))
-                if ip_in_net(ip_address,net):
-                    print fileinput.filename()+ " (" + last_aclname + "):" + line,
-                    break
-                m = p.search(line, m.start() + 1)
+        grepper.grep(line)
--- tests/patterns.py
+++ tests/patterns.py
@@ -6,46 +6,48 @@ import unittest
 
 # import aclgrep code from one directory above
 sys.path.append("..")
-from aclgrep import ip_and_mask_to_pair, ip_and_cidr_to_pair, ip_to_bits, ip_in_net
+from aclgrep import ACLGrepper
 
 class patterns(unittest.TestCase):
+
     def setUp(self):
-        pass
+        # the parameters do not matter for the generic tests
+        self.ag = ACLGrepper("1.1.1.1", False)
 
     def testIpToBits(self):
         # go over range
         for x in range(0,256):
             ip = ("%d.%d.%d.%d" % (x,x,x,x))
             value = x * 0x1000000 + x * 0x10000 + x * 0x100 + x
-            self.assertEqual(value, ip_to_bits(ip))
+            self.assertEqual(value, self.ag.ip_to_bits(ip))
 
         # corner cases
-        self.assertRaises(ValueError, ip_to_bits, "256.0.0.0")
-        self.assertRaises(ValueError, ip_to_bits, "a")
-        self.assertRaises(ValueError, ip_to_bits, "")
+        self.assertRaises(ValueError, self.ag.ip_to_bits, "256.0.0.0")
+        self.assertRaises(ValueError, self.ag.ip_to_bits, "a")
+        self.assertRaises(ValueError, self.ag.ip_to_bits, "")
 
     def testIpMaskPair(self):
         # check values
-        self.assertEqual((0x0a000000, 0xff000000), ip_and_mask_to_pair("10.0.0.0 255.0.0.0"))
-        self.assertEqual((0xc0a80200, 0xfffffc00), ip_and_mask_to_pair("192.168.2.0 255.255.252.0"))
+        self.assertEqual((0x0a000000, 0xff000000), self.ag.ip_and_mask_to_pair("10.0.0.0 255.0.0.0"))
+        self.assertEqual((0xc0a80200, 0xfffffc00), self.ag.ip_and_mask_to_pair("192.168.2.0 255.255.252.0"))
         # separator should not matter
-        self.assertEqual(ip_and_mask_to_pair("192.168.2.0 255.255.255.0"), ip_and_mask_to_pair("192.168.2.0/255.255.255.0"))
+        self.assertEqual(self.ag.ip_and_mask_to_pair("192.168.2.0 255.255.255.0"), self.ag.ip_and_mask_to_pair("192.168.2.0/255.255.255.0"))
         # equivalent subnet mask and wildcard mask
-        self.assertEqual(ip_and_mask_to_pair("192.168.2.0 255.255.255.0"), ip_and_mask_to_pair("192.168.2.0 0.0.0.255"))
+        self.assertEqual(self.ag.ip_and_mask_to_pair("192.168.2.0 255.255.255.0"), self.ag.ip_and_mask_to_pair("192.168.2.0 0.0.0.255"))
 
         # full bits -> interpret as host TODO: is this correct?
-        self.assertEqual((0x0a020304, 0xffffffff), ip_and_mask_to_pair("10.2.3.4 255.255.255.255"))
+        self.assertEqual((0x0a020304, 0xffffffff), self.ag.ip_and_mask_to_pair("10.2.3.4 255.255.255.255"))
         # no bits -> host
-        self.assertEqual((0x0a010101, 0xffffffff), ip_and_mask_to_pair("10.1.1.1/0.0.0.0"))
+        self.assertEqual((0x0a010101, 0xffffffff), self.ag.ip_and_mask_to_pair("10.1.1.1/0.0.0.0"))
 
     def testIpCidrPair(self):
         # check values
-        self.assertEqual((0x0a000000, 0xff000000), ip_and_cidr_to_pair("10.0.0.0/8"))
-        self.assertEqual((0xc0a80200, 0xfffffc00), ip_and_cidr_to_pair("192.168.2.0/22"))
+        self.assertEqual((0x0a000000, 0xff000000), self.ag.ip_and_cidr_to_pair("10.0.0.0/8"))
+        self.assertEqual((0xc0a80200, 0xfffffc00), self.ag.ip_and_cidr_to_pair("192.168.2.0/22"))
 
     def testIpInNet(self):
-        self.assertTrue(ip_in_net(0x0a010101, (0x0a000000, 0xff000000)))
-        self.assertFalse(ip_in_net(0x0a010101, (0x0a000000, 0xffffff00)))
+        self.assertTrue(self.ag.ip_in_net(0x0a010101, (0x0a000000, 0xff000000)))
+        self.assertFalse(self.ag.ip_in_net(0x0a010101, (0x0a000000, 0xffffff00)))
 
 if __name__ == '__main__':
     unittest.main()
